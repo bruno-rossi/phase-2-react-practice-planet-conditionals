970df51102f491cc81ba1258d99da77b9f383183
Bruno Rossi <brunogabrielrossi@gmail.com>
2023-12-18 19:43:45 -0500

    Planet Conditionals

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,24 @@ import PlanetsContainer from "./PlanetsContainer"
 
 function App() {
 
-  const [planets, setPlanets] = useState('')
+  const [planets, setPlanets] = useState([])
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
 
   useEffect(() => {
     console.log('fetching planets...')
     fetch('http://localhost:4001/planets')
     .then(res => res.json())
     .then(data => setPlanets(data))
-  })
+  }, [])
 
   return (
-    <div className="App">
+    <div className={isDarkMode ? "App" : "App light-mode"}>
 
       {/* You won't have to touch StarField, it's just here for visual things */}
       <StarField />
 
-      <button>Light Mode</button>
+      <button onClick={() => setIsDarkMode(!isDarkMode)}>{isDarkMode ? "Light mode" : "Dark mode"}</button>
 
       <PlanetsContainer planets={planets} />
 
--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PlanetFacts from "./PlanetFacts"
 
 function PlanetCard({planet}) {
 
+  const [ isToggled, setToggle ] = useState(false);
+
+  function handleToggle() {
+    setToggle(prevValue => !isToggled);
+    console.log(isToggled);
+  }
+
+  const isPlanetClass = planet.is_planet ? null : "not-planet";
+
   return (
-    <div className="card" onClick={() => console.log('Toggle!')}>
+    <div className="card" onClick={() => handleToggle()}>
       <h2>{planet.name}</h2>
-      <img className="" src={planet.image} alt={planet.name} />
+      { isToggled ? <PlanetFacts planet={planet} /> : <img className={isPlanetClass} src={planet.image} alt={planet.name} /> }
     </div>
   )
 }
